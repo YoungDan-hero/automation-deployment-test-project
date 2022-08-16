d2be65292e777d78423545ca8d6739dbc74c8302
YoungDan <1292867089@qq.com>
2022-08-16 12:04:04 +0800

    feat：push

--- auto.js
+++ auto.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const createHandler = require("github-webhook-handler");
+const ora = require("ora");
+
 const handler = createHandler({
     path: "/webhook",
     secret: "mySecretKiKoHashKey",
@@ -19,8 +21,11 @@ handler.on("error", function (err) {
 });
 
 handler.on("push", function (event) {
+    const loading = ora("正在全力构建项目");
+    loading.start();
     run_cmd("sh", ["./auto.sh"], function (text) {
-        console.log(text);
+        loading.stop();
+        console.log("自动构建成功");
     }); // 执行autoBuild.sh
 });
 
