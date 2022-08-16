0a5ed4bea0aa424c2f5ff838371f23063226974e
YoungDan <1292867089@qq.com>
2022-08-16 12:24:48 +0800

    feat:

--- auto.js
+++ auto.js
@@ -1,7 +1,6 @@
-import http from "http";
-import createHandler from "github-webhook-handler";
-import ora from "ora";
-import { spawn } from "child_process";
+const http = require("http");
+const createHandler = require("github-webhook-handler");
+
 const handler = createHandler({
     path: "/webhook",
     secret: "mySecretKiKoHashKey",
@@ -13,7 +12,6 @@ http.createServer(function (req, res) {
         res.end("no such location");
     });
 
-    console.log("触发了嘻嘻a");
 }).listen(7777);
 
 handler.on("error", function (err) {
@@ -21,22 +19,19 @@ handler.on("error", function (err) {
 });
 
 handler.on("push", function (event) {
-    const loading = ora("正在全力构建项目");
-    loading.start();
     run_cmd("sh", ["./auto.sh"], function (text) {
-        loading.stop();
-        console.log("自动构建成功");
-    }); // 执行autoBuild.sh
+        console.log("构建成功！");
+    });
 });
 
 const run_cmd = (cmd, args, callback) => {
+    const spawn = require("child_process").spawn;
     const child = spawn(cmd, args);
     let resp = "";
 
     child.stdout.on("data", function (buffer) {
         resp += buffer.toString();
     });
-
     child.stdout.on("end", function () {
         callback(resp);
     });
