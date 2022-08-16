002f8656ca0b0127b8a29a479bffe8917dea08f6
YoungDan <1292867089@qq.com>
2022-08-16 11:09:37 +0800

    feat：update

--- auto.js
+++ auto.js
@@ -14,25 +14,22 @@ app.all("/", (request, response) => {
     //接受所有响应头信息
     response.setHeader("Access-Control-Allow-Headers", "*");
 
-    // handler(request, response, function (err) {
-
-    // });
-
-    handler.on("error", function (err) {
-        console.error("Error:", err.message);
+    handler(request, response, function (err) {
+        response.statusCode = 200;
+        response.end("success");
     });
 
-    handler.on("push", function (event) {
-        console.log(
-            "Received a push event for %s to %s",
-            event.payload.repository.name,
-            event.payload.ref
-        );
-    });
+    console.log("触发了服务");
+});
 
-    console.log(1);
+handler.on("error", function (err) {
+    console.error("Error:", err.message);
+});
+
+handler.on("push", function (event) {
+    console.log("Received a push event for %s to %s");
 });
 
 app.listen(7777, () => {
-    console.log("启动");
+    console.log("服务启动，监听到7777端口!");
 });
