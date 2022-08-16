0018d9bf9de75389515becd0136fbb63dcb6e751
YoungDan <1292867089@qq.com>
2022-08-16 11:32:45 +0800

    feat：update

--- auto.js
+++ auto.js
@@ -1,31 +1,28 @@
-import express from "express";
-const app = express();
-import createHandler from "github-webhook-handler";
-
+const http = require("http");
+const createHandler = require("github-webhook-handler");
 const handler = createHandler({
     path: "/webhook",
     secret: "mySecretKiKoHashKey",
 });
 
-//接受所有请求方式
-app.all("/", (request, response) => {
-    //设置响应头允许跨域
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    //接受所有响应头信息
-    response.setHeader("Access-Control-Allow-Headers", "*");
-    console.log(handler);
-    console.log(request);
-    handler(request, response, function (err) {
-        console.log("触发了服务");
-        response.statusCode = 200;
-        response.end("success");
+http.createServer(function (req, res) {
+    
+    handler(req, res, function (err) {
+        res.statusCode = 200;
+        res.end("no such location");
     });
+
+    console.log("触发了");
+}).listen(7777);
+
+handler.on("error", function (err) {
+    console.error("Error:", err.message);
 });
 
 handler.on("push", function (event) {
-    console.log("Received a push event for %s to %s");
-});
-
-app.listen(7777, () => {
-    console.log("服务启动，监听到7777端口!");
+    console.log(
+        "Received a push event for %s to %s",
+        event.payload.repository.name,
+        event.payload.ref
+    );
 });
